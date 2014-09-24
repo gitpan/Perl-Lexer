250cd174e90bf7d0c7537e4048e4bc25a3b1d478
Kenichi Ishigaki <ishigaki@cpan.org>
2014-09-24 05:24:32 +0000

    Import of ISHIGAKI/Perl-Lexer-0.08 from CPAN.
    gitpan-cpan-distribution: Perl-Lexer
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         ISHIGAKI/Perl-Lexer-0.08.tar.gz
    gitpan-cpan-author:       ISHIGAKI
    gitpan-cpan-maturity:     released

--- lib/Perl/gen/token_info_map.h
+++ lib/Perl/gen/token_info_map.h
@@ -144,11 +144,17 @@
 #include "token_info-5.19.9.h"
 #elif PERL_VERSION == 20 && PERL_SUBVERSION == 0
 #include "token_info-5.20.0.h"
+#elif PERL_VERSION == 20 && PERL_SUBVERSION == 1
+#include "token_info-5.20.0.h"
 #elif PERL_VERSION == 21 && PERL_SUBVERSION == 0
 #include "token_info-5.21.0.h"
 #elif PERL_VERSION == 21 && PERL_SUBVERSION == 1
 #include "token_info-5.21.1.h"
-#elif PERL_VERSION > 21 || (PERL_VERSION == 21 && PERL_SUBVERSION > 1)
+#elif PERL_VERSION == 21 && PERL_SUBVERSION == 2
+#include "token_info-5.21.1.h"
+#elif PERL_VERSION == 21 && PERL_SUBVERSION == 4
+#include "token_info-5.21.4.h"
+#elif PERL_VERSION > 21 || (PERL_VERSION == 21 && PERL_SUBVERSION > 4)
 #include "token_info-latest.h"
 #else
 #error "No support for this perl version"
